1b218ce3d63140e57425827464c3b9b1faa0b272
Neoprot <kaua.seichi.gomes@gmail.com>
2024-09-11 19:36:05 -0300

    feat: adding new modal for sharing favorites

---
+++ client/src/components/ShareModal.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+
+interface ShareModalProps {
+    isOpen: boolean;
+    shareableLink: string;
+    onClose: () => void;
+}
+
+const ShareModal: React.FC<ShareModalProps> = ({ isOpen, shareableLink, onClose }) => {
+    if (!isOpen) return null;
+
+    const handleCopyToClipboard = () => {
+        navigator.clipboard.writeText(shareableLink);
+        onClose();
+    };
+
+    const handleShareWhatsApp = () => {
+        const whatsappUrl = `https://wa.me/?text=${encodeURIComponent(shareableLink)}`;
+        window.open(whatsappUrl, '_blank');
+        onClose();
+    };
+
+    return (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50" onClick={onClose}>
+            <div
+                className="bg-white p-6 rounded-md shadow-md text-center"
+                onClick={(e) => e.stopPropagation()} // Evita fechar o modal ao clicar dentro
+            >
+                
+
+                <h2 className="text-xl font-bold mb-4 text-black">Share your favorites</h2>
+                <div className="text-green-500 text-3xl mb-4">✔️</div>
+                <p className="text-gray-700 mb-4">Copy the link or share it via WhatsApp:</p>
+
+                <div className="flex justify-between mb-4 gap-x-4   	">
+                    <button
+                        onClick={handleCopyToClipboard}
+                        className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+                    >
+                        Copy to clipboard
+                    </button>
+                    <button
+                        onClick={handleShareWhatsApp}
+                        className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600"
+                    >
+                        Share via WhatsApp
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ShareModal;
--- client/src/pages/FavoritesPage.tsx
+++ client/src/pages/FavoritesPage.tsx
@@ -4,23 +4,25 @@ import FavoriteList from '../components/FavoriteList';
 import { useAuth } from '../context/AuthContext';
 import { postSharedFavorites, hasSharedFavorites, deleteSharedFavorites } from '../services/api';
 import { useFavorites } from '../context/FavoritesContext';
-import LoadingModal from '../components/LoadingModal';  // Importa o componente do modal
+import LoadingModal from '../components/LoadingModal';
+import ShareModal from '../components/ShareModal'; 
 
 const FavoritesPage: React.FC = () => {
     const { favorites } = useFavorites();
     const { user, logout } = useAuth();
     const [id, setId] = useState('');
-    const [hasSharedFavoritesState,sethasSharedFavoritesState] = useState(false);
+    const [hasSharedFavoritesState, sethasSharedFavoritesState] = useState(false);
     const [isLoading, setIsLoading] = useState(false);
     const [message, setMessage] = useState('');
     const [success, setSuccess] = useState(false);
+    const [isShareModalOpen, setIsShareModalOpen] = useState(false);
+    const [shareableLink, setShareableLink] = useState('');
 
     useEffect(() => {
         async function fetchData() {
             if (!user) return;
             const res = await hasSharedFavorites(user.id);
             setId(res.id);
-            console.log(res.id);
             if (res.id) {
                 sethasSharedFavoritesState(true);
             }
@@ -36,24 +38,23 @@ const FavoritesPage: React.FC = () => {
         try {
             const moviesId = favorites.map((movie) => movie.movie_id || movie.id);
             const res = await postSharedFavorites(user.id, user.username, user.email, moviesId);
+            const link = `${window.location.origin}/shared-favorites/${res.id}`;
+            setId(res.id);
+            setShareableLink(link);
             setSuccess(true);
-            setMessage(`Shareable link: ${window.location.origin}/shared-favorites/${res.id}. 
-                In 5 seconds this window will be automatic closed.`);
+            setMessage('Shareable link created!');
 
-            const redirect = window.confirm('Do you want to be redirected to the new page?');
-            if (redirect) {
-                window.location.href = `/shared-favorites/${res.id}`;
-            }
-            setTimeout(() => {
-                setIsLoading(false);
-                sethasSharedFavoritesState(true);
-            }, 5000);
+            sethasSharedFavoritesState(true);
+            setIsShareModalOpen(true); 
         } catch (error) {
             setSuccess(false);
             setMessage('Error sharing favorites. Please try again.');
             console.error('Error sharing favorites:', error);
+        } finally {
+            setIsLoading(false);
         }
     };
+
     const handleUnshareFavorites = async () => {
         try {
             setIsLoading(true);
@@ -70,27 +71,21 @@ const FavoritesPage: React.FC = () => {
             console.error('Error unsharing favorites:', error);
         }
     };
-    const goToSharedFavorites = (userId: any) => {
-        console.log(userId);
-        window.location.href = `/shared-favorites/${userId}`;
-    };	
 
     return (
         <div className="min-h-screen flex flex-col bg-gray-900 text-white">
-            <header className="px-20 py-6 bg-blue-500 text-white flex justify-between items-center" onClick={()=>console.log(favorites.length)}>
+            <header className="px-20 py-6 bg-blue-500 text-white flex justify-between items-center">
                 <Link to="/">
                     <img src="/logo.png" alt="Logo" className="h-20" />
                 </Link>
                 <div className="flex items-center space-x-6">
-                    {user ? (
+                    {user && (
                         <>
                             <span className="ml-8">{user.email}</span>
                             <button onClick={logout} className="text-white hover:underline">
                                 Logout
                             </button>
                         </>
-                    ) : (
-                        <></>
                     )}
                 </div>
             </header>
@@ -98,15 +93,16 @@ const FavoritesPage: React.FC = () => {
                 <div className="flex justify-between items-center mb-6">
                     <h1 className="text-4xl font-bold font-serif">Your Favorite Movies</h1>
                     {!hasSharedFavoritesState ? (
-                        <button 
-                        onClick={handleShareFavorites} 
-                        className="bg-blue-800 text-white px-4 py-2 rounded"
-                    >
-                        Share your favorites Movies
-                    </button>
-                ) :(
-                    <><button
-                                onClick={()=>goToSharedFavorites(id)}
+                        <button
+                            onClick={handleShareFavorites}
+                            className="bg-blue-800 text-white px-4 py-2 rounded"
+                        >
+                            Share your favorites Movies
+                        </button>
+                    ) : (
+                        <>
+                            <button
+                                onClick={() => window.location.href = `/shared-favorites/${id}`}
                                 className="bg-blue-800 text-white px-4 py-2 rounded"
                             >
                                 Go to your shareable link
@@ -115,11 +111,12 @@ const FavoritesPage: React.FC = () => {
                                 onClick={handleUnshareFavorites}
                                 className="bg-blue-800 text-white px-4 py-2 rounded"
                             >
-                                    Unshare your favorites Movies
-                                </button></>
-                )}
+                                Unshare your favorites Movies
+                            </button>
+                        </>
+                    )}
                 </div>
-                <div className='mx-12'>
+                <div className="mx-12">
                     <FavoriteList />
                 </div>
             </main>
@@ -127,7 +124,8 @@ const FavoritesPage: React.FC = () => {
                 <p>© 2024 Cinelist. All rights reserved.</p>
             </footer>
 
-            <LoadingModal isLoading={isLoading} message={message} success={success} error={false}/>
+            <LoadingModal isLoading={isLoading} message={message} success={success} error={false} />
+            <ShareModal isOpen={isShareModalOpen} shareableLink={shareableLink} onClose={() => setIsShareModalOpen(false)} />
         </div>
     );
 };
