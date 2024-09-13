0f3e6fd707c3f475ad21dd53a33848b2f7dea764
Neoprot <kaua.seichi.gomes@gmail.com>
2024-09-13 12:49:10 -0300

    fix:separating sharedfavorites functions

--- server/app.ts
+++ server/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import authRoutes from "./routes/authRoutes";
 import movieRoutes from "./routes/movieRoutes";
+import sharedRoutes from "./routes/sharedRoutes";
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(express.json());
 
 app.use("/auth", authRoutes);
 app.use("/movies", movieRoutes);
+app.use("/shared", sharedRoutes);
 
 app.get("/", (req, res) => {
   res.redirect(`http://localhost:3000/`);
